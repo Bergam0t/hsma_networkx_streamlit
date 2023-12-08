70d188674ed490cdb292bee999ab6d4179c29c28
Bergam0t <s.j.rosser@exeter.ac.uk>
2023-12-08 12:02:06 +0000

    Update to use st_cytoscape_extra package and new layouts it adds

--- pages/4_networkx_Example_4:_Complex_Cytoscape.py
+++ pages/4_networkx_Example_4:_Complex_Cytoscape.py
@@ -4,7 +4,9 @@ import numpy as np
 import streamlit as st
 from helper_functions import add_logo
 import gc
-from st_cytoscape import cytoscape
+# from st_cytoscape import cytoscape
+from st_cytoscape_extra import cytoscape
+import dash_cytoscape as cyto
 
 st.set_page_config(
     page_title="Interactive 2D Example - Complex",
@@ -12,6 +14,7 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded",
 )
+
 
 add_logo()
 
@@ -145,178 +148,255 @@ st.markdown(
     Communities were detected using Clauset-Newman-Moore modularity maximisation. 
     
     [Source](https://networkx.org/documentation/stable/reference/algorithms/generated/networkx.algorithms.community.modularity_max.greedy_modularity_communities.html) 
-    
-    Filtering is done on the weight of the edges (in this case, number of interactions between characters)
-
-    If there are no remaining links after filtering, the node is removed from the dataset.
-
-
-    **Filtering should be used with caution.** 
-    
-    In a directional graph, filtering of weights is done per direction and could lead to some very misleading outputs - for example, if A --> B has a weight of 99, but B --> A has a weight of 101 and your threshold is 100, you'd lose the A --> B connection while it would look like B --> A mattered.
-    
-    In an undirected graph, it may give the impression that two characters have never interacted with each other.
-
-    
-    
-    Game of thrones data from [this repo](https://github.com/hsma-programme/4d_advanced_network_analysis_pt2/tree/main/data)
-    """
-)
-
-
-
-# add streamlit inputs
-layout = st.radio(label="Select layout",
-                  options=["fcose", "circle", "random", "grid", "concentric",
-                           "breadthfirst", "cose", "klay"],
-                  horizontal=True)
-
-min_threshold_weight = st.slider(
-    "Filter out edges that don't meet this threshold weight", 
-    int(1),
-    int(edges["Weight"].max())
-    )
-
-# Define the edge filter 
-def filter_edge(n1, n2):
-
-    return G[n1][n2]['Weight'] >= min_threshold_weight
-
-G2 = nx.subgraph_view(G, 
-                     filter_edge=filter_edge)
-
-# Remove any nodes that now don't meet the weight threshold
-# Have to take a copy of the graph first because the output of the 
-# filter step is read only
-G3 = G2.copy()
-G3.remove_nodes_from(list(nx.isolates(G3)))
-
-
-
-min_total_interactions = st.slider(
-    "Filter out characters with fewer than a threshold number of interactions", 
-    int(1),
-    int(nodes["TotalInteractions"].max())
-    )
-
-# Define the edge filter 
-def filter_node(n1):
-
-    return G.nodes[n1]['TotalInteractions'] >= min_total_interactions
-
-G4 = nx.subgraph_view(G3, 
-                     filter_node=filter_node)
-
-
-G_cs = nx.cytoscape_data(G4)
-
-bb = nx.betweenness_centrality(G4).values()
-
-elements = G_cs['elements']
-
-# This is a nice example of how to adjust the stylesheet
-# https://github.com/cytoscape/cytoscape.js/blob/master/documentation/demos/colajs-graph/cy-style.json
-stylesheet = [
-    {
-        "selector": "node", 
-        "style": {
-            "label": "data(Label)", 
-            "width": f"mapData(Size, {min(bb)}, {max(bb)}, 3, 15)", 
-            "height": f"mapData(Size, {min(bb)}, {max(bb)}, 3, 15)",
-            "font-size": "8px",
-            "background-color": "data(CommunityColor)"
-            
-            }
-        },
-
-    {
-        "selector": "edge",
-        "style": {
-            "width": f'mapData(Weight, 1, {edges["Weight"].max()}, 0.1, 5)',
-            "curve-style": "bezier",
-            "opacity": f'mapData(Weight, 0, {edges["Weight"].max()}, 0.4, 1)'
-            #"target-arrow-shape": "triangle",
-            #"arrow-scale": f'mapData(Weight, 1, {edges["Weight"].max()}, 0.1, 1)'
-        },
-    },
-
-    # {
-    #     "layout": {
-    #         # 'EdgeLength': length,
-    #         # 'maxSimulationTime': 8000,
-    #         # 'convergenceThreshold': 0.001,
-    #         # 'nodeOverlap': 20,
-    #         # 'refresh': 20,
-    #         # 'fit': True,
-    #         # 'padding': 30,
-    #         # 'randomize': True,
-    #         # 'componentSpacing': 100,
-    #         'nodeRepulsion': 400,
-    #         # 'edgeElasticity': 100000,
-    #         # 'nestingFactor': 5,
-    #         # 'gravity': 80,
-    #         # 'numIter': 1000,
-    #         # 'initialTemp': 200,
-    #         # 'coolingFactor': 0.95,
-    #         # 'minTemp': 1.0
-    #     }
-    # }
-]
-
-
-# Deal with the output on the page
-
-st.subheader("Interactions between characters in Game of Thrones - Series 1")
-
-layout2 = st.radio(label="Select layout of filtered graph",
-                  options=["fcose", "circle", "random", "grid", "concentric",
-                           "breadthfirst", "cose", "klay"],
-                  horizontal=True)
-
-
-
-selected = cytoscape(elements, 
-                     stylesheet, 
-                     key="graph", 
-                     layout={"name": layout}, 
-                     height="900px")
-
-st.markdown(f"""
-    Links representing fewer than {min_threshold_weight} interactions have been removed from this graph. 
-    
-    Characters with fewer than than {min_total_interactions} interactions have been removed from this graph. 
-
-    {len(G4.nodes)} of {len(nodes)} nodes in original dataset displayed after filtering.
-
-    **Interpret with caution.**
-
     """)
 
 
+tab1, tab2, tab3, tab4 = st.tabs(["Edge Weight and Total Interaction Filtering", 
+                            "Filter to Node Neighbourhood",
+                    "Minimum Spanning Trees Pruning", 
+                    "Network Metrics"])
 
+layout_options_list = ["fcose", "circle", "random", "grid", "concentric",
+                           "breadthfirst", "cose", "klay", 
+                           "avsdf", "elk","dagre", "cola"
+                        , "cise"
+                           ]
+with tab1:
+    st.markdown("""
+        Filtering is done on the weight of the edges (in this case, number of interactions between characters)
+
+        If there are no remaining links after filtering, the node is removed from the dataset.
+
+
+        **Filtering should be used with caution.** 
+        
+        In a directional graph, filtering of weights is done per direction and could lead to some very misleading outputs - for example, if A --> B has a weight of 99, but B --> A has a weight of 101 and your threshold is 100, you'd lose the A --> B connection while it would look like B --> A mattered.
+        
+        In an undirected graph, it may give the impression that two characters have never interacted with each other.
+
+        
+        
+        Game of thrones data from [this repo](https://github.com/hsma-programme/4d_advanced_network_analysis_pt2/tree/main/data)
+        """
+    )
+
+
+
+    # add streamlit inputs
+    layout = st.radio(label="Select layout",
+                    options=layout_options_list,
+                    horizontal=True)
+
+    min_threshold_weight = st.slider(
+        "Filter out edges that don't meet this threshold weight", 
+        int(1),
+        int(edges["Weight"].max())
+        )
+
+    # Define the edge filter 
+    def filter_edge(n1, n2):
+
+        return G[n1][n2]['Weight'] >= min_threshold_weight
+
+    G2 = nx.subgraph_view(G, 
+                        filter_edge=filter_edge)
+
+    # Remove any nodes that now don't meet the weight threshold
+    # Have to take a copy of the graph first because the output of the 
+    # filter step is read only
+    G3 = G2.copy()
+    G3.remove_nodes_from(list(nx.isolates(G3)))
+
+
+
+    min_total_interactions = st.slider(
+        "Filter out characters with fewer than a threshold number of interactions", 
+        int(1),
+        int(nodes["TotalInteractions"].max())
+        )
+
+    # Define the edge filter 
+    def filter_node(n1):
+
+        return G.nodes[n1]['TotalInteractions'] >= min_total_interactions
+
+    G4 = nx.subgraph_view(G3, 
+                        filter_node=filter_node)
+
+
+    G_cs = nx.cytoscape_data(G4)
+
+    bb = nx.betweenness_centrality(G4).values()
+
+    elements = G_cs['elements']
+
+    # This is a nice example of how to adjust the stylesheet
+    # https://github.com/cytoscape/cytoscape.js/blob/master/documentation/demos/colajs-graph/cy-style.json
+    stylesheet = [
+        {
+            "selector": "node", 
+            "style": {
+                "label": "data(Label)", 
+                "width": f"mapData(Size, {min(bb)}, {max(bb)}, 3, 15)", 
+                "height": f"mapData(Size, {min(bb)}, {max(bb)}, 3, 15)",
+                "font-size": "8px",
+                "background-color": "data(CommunityColor)",
+                # "transparency": f'mapData(Weight, 0, {max(bb)}, 0.4, 1)'
+                }
+            },
+
+        {
+            "selector": "edge",
+            "style": {
+                "width": f'mapData(Weight, 1, {edges["Weight"].max()}, 0.1, 5)',
+                "curve-style": "bezier",
+                "opacity": f'mapData(Weight, 0, {edges["Weight"].max()}, 0.4, 1)'
+                #"target-arrow-shape": "triangle",
+                #"arrow-scale": f'mapData(Weight, 1, {edges["Weight"].max()}, 0.1, 1)'
+            },
+        },
+
+        # {
+        #     "layout": {
+        #         # 'EdgeLength': length,
+        #         # 'maxSimulationTime': 8000,
+        #         # 'convergenceThreshold': 0.001,
+        #         # 'nodeOverlap': 20,
+        #         # 'refresh': 20,
+        #         # 'fit': True,
+        #         # 'padding': 30,
+        #         # 'randomize': True,
+        #         # 'componentSpacing': 100,
+        #         'nodeRepulsion': 400,
+        #         # 'edgeElasticity': 100000,
+        #         # 'nestingFactor': 5,
+        #         # 'gravity': 80,
+        #         # 'numIter': 1000,
+        #         # 'initialTemp': 200,
+        #         # 'coolingFactor': 0.95,
+        #         # 'minTemp': 1.0
+        #     }
+        # }
+    ]
+
+
+    # Deal with the output on the page
+
+    st.subheader("Interactions between characters in Game of Thrones - Series 1")
+
+    selected = cytoscape(elements, 
+                        stylesheet, 
+                        key="graph", 
+                        layout={"name": layout}, 
+                        height="900px")
+
+    st.markdown(f"""
+        Links representing fewer than {min_threshold_weight} interactions have been removed from this graph. 
+        
+        Characters with fewer than than {min_total_interactions} interactions have been removed from this graph. 
+
+        {len(G4.nodes)} of {len(nodes)} nodes in original dataset displayed after filtering.
+
+        **Interpret with caution.**
+
+        """)
 
 # Add ability to filter down to just the network for an individual
+with tab2: 
+    st.subheader("Show full network for particular characters")
 
-st.subheader("Show full network for particular characers")
+    layout2 = st.radio(label="Select layout of filtered graph",
+                    options=layout_options_list,
+                    horizontal=True)
 
-character_filter = st.selectbox("Select characters to include",
-             options=nodes['ID'].drop_duplicates().tolist(),
-             format_func=lambda x: x.title().replace("_", " "))
-
-
-def filter_node_neighbour(n1):
-
-    return (character_filter in nx.all_neighbors(G, n1)) or (n1 == character_filter)
-
-
-G5 = nx.subgraph_view(G, 
-                     filter_node=filter_node_neighbour)
+    character_filter = st.selectbox("Select characters to include (ordered in descending order starting with characters with the most total interactions)",
+                options=nodes.sort_values("TotalInteractions", ascending=False)['ID'].drop_duplicates().tolist(),
+                format_func=lambda x: x.title().replace("_", " "))
 
 
-bb = nx.betweenness_centrality(G5).values()
+    def filter_node_neighbour(n1):
 
-selected = cytoscape(nx.cytoscape_data(G5)['elements'], 
-                     stylesheet, 
-                     key="graph_neighbour", 
-                     layout={"name": layout2}, 
-                     height="900px")
+        return (character_filter in nx.all_neighbors(G, n1)) or (n1 == character_filter)
+
+
+    G5 = nx.subgraph_view(G, 
+                        filter_node=filter_node_neighbour)
+
+
+    bb = nx.betweenness_centrality(G5).values()
+
+    stylesheet = [
+        {
+            "selector": "node", 
+            "style": {
+                "label": "data(Label)", 
+                "width": f"mapData(Size, {min(bb)}, {max(bb)}, 3, 15)", 
+                "height": f"mapData(Size, {min(bb)}, {max(bb)}, 3, 15)",
+                "font-size": "8px",
+                "background-color": "data(CommunityColor)",
+                # "transparency": f'mapData(Weight, 0, {max(bb)}, 0.4, 1)'
+                }
+            },
+
+        {
+            "selector": "edge",
+            "style": {
+                "width": f'mapData(Weight, 1, {edges["Weight"].max()}, 0.1, 5)',
+                "curve-style": "bezier",
+                "opacity": f'mapData(Weight, 0, {edges["Weight"].max()}, 0.4, 1)'
+                #"target-arrow-shape": "triangle",
+                #"arrow-scale": f'mapData(Weight, 1, {edges["Weight"].max()}, 0.1, 1)'
+            },
+        }
+     ]
+
+    selected = cytoscape(nx.cytoscape_data(G5)['elements'], 
+                        stylesheet, 
+                        key="graph_neighbour", 
+                        layout={"name": layout2}, 
+                        height="900px")
+
+with tab3:
+    st.subheader("Pruning with Minimum Spanning Trees Algorithm")
+
+    layout3 = st.radio(label="Select layout for minimum spanning tree graph",
+                    options=layout_options_list,
+                    horizontal=True)
+
+    T = nx.minimum_spanning_tree(G)
+
+
+    stylesheet = [
+        {
+            "selector": "node", 
+            "style": {
+                "label": "data(Label)", 
+                "width": f"mapData(Size, {min(bb)}, {max(bb)}, 3, 15)", 
+                "height": f"mapData(Size, {min(bb)}, {max(bb)}, 3, 15)",
+                "font-size": "8px",
+                "background-color": "data(CommunityColor)",
+                # "transparency": f'mapData(Weight, 0, {max(bb)}, 0.4, 1)'
+                }
+            },
+
+        {
+            "selector": "edge",
+            "style": {
+                "width": f'mapData(Weight, 1, {edges["Weight"].max()}, 0.1, 5)',
+                "curve-style": "bezier",
+                "opacity": f'mapData(Weight, 0, {edges["Weight"].max()}, 0.4, 1)'
+                #"target-arrow-shape": "triangle",
+                #"arrow-scale": f'mapData(Weight, 1, {edges["Weight"].max()}, 0.1, 1)'
+            },
+        }
+     ]
+
+    selected = cytoscape(nx.cytoscape_data(T)['elements'], 
+                        stylesheet, 
+                        key="graph_mst", 
+                        layout={"name": layout3}, 
+                        height="900px")
+
+with tab4:
+    st.subheader("Graphs of network metrics")
+
